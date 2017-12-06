d7f35e693a01baa06d7d956fc33121de387d093e
Ryan Hunter <ryah.hunter@gmail.com>
2017-12-05 19:03:03 -0600

    Update BookView.java

--- BookView.java
+++ BookView.java
@@ -1,29 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package pkg360.practice;
 
-
-public class BookView {
+/**
+ *
+ * @author Ryan
+ */
+public class BookView implements View{
     
-	// Default view
-    public void printDefault(){
-      System.out.println("********************************\n" +
-                         "Welcome to book database\n" +
+    @Override
+    public void display(){
+        
+    System.out.println("********************************\n" +
+                         "Booklist\n" +
                          "********************************");
-    }
     
-	// Asks for title
-    public void askForTitle(){
-      System.out.println("Please enter title:");
-    }
-    
-	// Asks for author
-    public void askForAuthor(){
-      System.out.println("Please enter author:");
-    }
-    
-	// Displays book info
-    public void printBookDetails(String bookTitle, String bookAuthor){
-      System.out.println("Book: ");
-      System.out.println("Title: " + bookTitle);
-      System.out.println("Author: " + bookAuthor);
+      System.out.println("A Farewell to Arms, Ernest Hemingway");
+      System.out.println("As I Lay Dying, William Faulkner");
+      System.out.println("Ulysses, James Joyce");
+      System.out.println("The Hobbit, J R R Tolkien");
+      System.out.println("Dune, Frank Herbert");
    }
 }
